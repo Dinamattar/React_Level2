fe659d5accf9706d8bb59e12c1e5dbb8ef88358f
Dinamattar <dinamatar2002@gmail.com>
2026-09-24 22:47:35 +0300

    session6

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,60 @@
+import { Link } from "react-router-dom";
 import "./App.css";
 import "./theme.css";
-import { useState } from "react";
+import { useReducer } from "react";
+
+// import { useState } from "react";
 function App() {
-  const [field, setField] = useState("Front End");
-  const [year, setYear] = useState("3");
-  const [count, setCount] = useState(0);
-  const [color, setColor] = useState(" ");
+  // const [field, setField] = useState("Front End");
+  // const [year, setYear] = useState("3");
+  // const [count, setCount] = useState(0);
+  // const [color, setColor] = useState(" ");
+
+  const initialScore = { field: "Fornt End", year: 3, count: 0, color: "" };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "CHANGE_FIELD":
+        return { ...state, field: action.newValue };
+      case "CHANGE_YEAR":
+        return { ...state, year: action.newValue };
+      case "CHANGE_COUNT":
+        return { ...state, count: action.newValue };
+      case "CHANGE_COLOR":
+        return { ...state, color: action.newValue };
+      default:
+        return state;
+    }
+  };
+  const [allData, dispatch] = useReducer(reducer, initialScore);
 
   return (
-    <div className={`App ${color}`}>
+    <div className={`App ${allData.color}`}>
+      <Link to="/page2">Go To Page 2</Link>
+      <br />
       <button
         style={{ marginBottom: 60 }}
-        onClick={() => setColor(color == "" ? "dark" : "")}
+        onClick={() =>
+          dispatch({
+            type: "CHANGE_COLOR",
+            newValue: allData.color == "" ? "dark" : "",
+          })
+        }
       >
-        Tgggle Theme
-      </button>.
-      <input type="checkbox" id="toggle_checkbox"  onChange={() => {
-          setColor(color == "" ? "dark" : "");
-        }}/>
-      <label
-        htmlFor="toggle_checkbox"
-      >
+        Toggle Theme
+      </button>
+      .
+      <input
+        type="checkbox"
+        id="toggle_checkbox"
+        onChange={() => {
+          dispatch({
+            type: "CHANGE_COLOR",
+            newValue: allData.color == "" ? "dark" : "",
+          });
+        }}
+      />
+      <label htmlFor="toggle_checkbox">
         <div id="star">
           <div className="star" id="star-1">
             ★
@@ -35,31 +69,49 @@ function App() {
         <button
           className="light"
           style={{ marginRight: 26 }}
-          onClick={() => setColor("light")}
+          onClick={() => dispatch({ type: "CHANGE_COLOR", newValue: "light" })}
         >
           light
         </button>
         <button
           className="dark"
           style={{ marginRight: 26 }}
-          onClick={() => setColor("dark")}
+          onClick={() => dispatch({ type: "CHANGE_COLOR", newValue: "dark" })}
         >
           dark
         </button>
-        <button className="gray" onClick={() => setColor("gray")}>
+        <button
+          className="gray"
+          onClick={() => dispatch({ type: "CHANGE_COLOR", newValue: "gray" })}
+        >
           gray
         </button>
-        <button className="pink" onClick={() => setColor("pink")}>
+        <button
+          className="pink"
+          onClick={() => dispatch({ type: "CHANGE_COLOR", newValue: "pink" })}
+        >
           pink
         </button>
       </div>
-      <h2>My Field is {field}</h2>
-      <button onClick={() => setField("Back End")}>Change Field</button>
+      <h2>My Field is {allData.field} </h2>
+      <button
+        onClick={() => dispatch({ type: "CHANGE_FIELD", newValue: "Back End" })}
+      >
+        Change Field
+      </button>
       <br />
-      <h2>My Experience Year is {year}</h2>
-      <button onClick={() => setYear("5")}>Change Year</button>
+      <h2>My Experience Year is {allData.year} </h2>
+      <button onClick={() => dispatch({ type: "CHANGE_YEAR", newValue: 5 })}>
+        Change Year
+      </button>
       <br />
-      <button onClick={() => setCount(count + 1)}>Counter is {count}</button>
+      <button
+        onClick={() =>
+          dispatch({ type: "CHANGE_COUNT", newValue: allData.count + 1 })
+        }
+      >
+        Counter is {allData.count}
+      </button>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Page2 from './Page2.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/page2",
+    element: <Page2 />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
